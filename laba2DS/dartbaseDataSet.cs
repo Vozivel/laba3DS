a9cf01d8427f2a100cd52efacc6ae27de2b38846
lidiia <lidiia@LIDA>
2024-04-05 10:51:28 +0300

    Добавьте файлы проекта.

---
+++ laba2DS/dartbaseDataSet.cs
@@ -0,0 +1,15 @@
+﻿namespace laba2DS
+{
+
+
+    partial class dartbaseDataSet
+    {
+    }
+}
+
+namespace laba2DS.dartbaseDataSetTableAdapters {
+    
+    
+    public partial class GamesTableAdapter {
+    }
+}
